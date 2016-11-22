46917b33fd56056b3254db192d7de67d23c8d482
Tolja Zubow <zubow@tkn.tu-berlin.de>
2016-11-22 10:22:22 +0100

    Example GnuRadio controller.

--- wifi/local_control_app.py
+++ wifi/local_control_app.py
@@ -6,10 +6,10 @@ from uniflex.core import events
 from uniflex.timer import TimerEventSender
 
 
-__author__ = "Piotr Gawlowicz"
+__author__ = "Anatolij Zubow, Piotr Gawlowicz"
 __copyright__ = "Copyright (c) 2016, Technische Universität Berlin"
 __version__ = "0.1.0"
-__email__ = "{gawlowicz}@tkn.tu-berlin.de"
+__email__ = "{zubow,gawlowicz}@tkn.tu-berlin.de"
 
 
 class PeriodicEvaluationTimeEvent(events.TimeEvent):
