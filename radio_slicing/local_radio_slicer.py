ba27c0d759147a7a539351db9cb725ffd7b0072c
Sven Zehl <zehl@tu-berlin.de>
2016-11-29 11:05:36 +0100

    Radio Slicer update

--- radio_slicing/local_radio_slicer.py
+++ radio_slicing/local_radio_slicer.py
@@ -5,10 +5,10 @@ from sbi.wifi.params import HMACConfigParam, HMACAccessPolicyParam
 from uniflex.core import modules
 from uniflex.core.timer import TimerEventSender
 
-__author__ = "Anatolij Zubow"
+__author__ = "Anatolij Zubow, Sven Zehl"
 __copyright__ = "Copyright (c) 2016, Technische Universität Berlin"
 __version__ = "0.1.0"
-__email__ = "{zubow}@tkn.tu-berlin.de"
+__email__ = "{zubow, zehl}@tkn.tu-berlin.de"
 
 '''
 A local controller program running on WiFi AP performing radio slicing (based on hMAC).
@@ -41,25 +41,31 @@ class LocalRadioSlicer(modules.ControlApplication):
         self.log.info(node)
         self.device = node.get_device(0)
         self.log.info(self.device)
-
         self.myHMACID = 'RadioSlicerID'
-        self.iface = 'ap1'
-        total_slots = 10
+        self.iface = 'ap5'
+        self.total_slots = 20
         # slots are in microseonds
         slot_duration = 20000  # 20 ms
+        
+        sta1 = "00:15:6d:86:0f:88" #fernseher
+        self.min_rates = {sta1 : 15.0}
+        self.phy_rates = {}
+        self.min_slots = {}
+
 
         # create new MAC for local node
         self.mac = HMACConfigParam(
-            no_slots_in_superframe=total_slots,
+            no_slots_in_superframe=self.total_slots,
             slot_duration_ns=slot_duration)
 
         # assign allow all to each slot
-        for slot_nr in range(total_slots):
+        for slot_nr in range(self.total_slots):
                 acGuard = HMACAccessPolicyParam()
                 acGuard.allowAll()  # allow all
                 self.mac.addAccessPolicy(slot_nr, acGuard)
-
+        self.mac.printConfiguration()
         # install configuration in MAC
+        #self.device.install_mac_processor(self.iface, self.mac)
         self.device.activate_radio_program(self.myHMACID, self.mac, self.iface)
 
         self.timer = TimerEventSender(self, PeriodicEvaluationTimeEvent)
@@ -73,48 +79,80 @@ class LocalRadioSlicer(modules.ControlApplication):
         self.log.info("stop wifi radio slicer")
 
         # install configuration in MAC
+        #self.device.uninstall_mac_processor(self.iface, self.mac)
         self.device.deactivate_radio_program(self.myHMACID)
-
 
     @modules.on_event(PeriodicEvaluationTimeEvent)
     def periodic_slice_adapdation(self, event):
         print("Periodic slice adaptations ...")
-
-        try:
-            # TODO: enable me!!!
-            if False:
+        if True:
+            if True:
                 # step 1: get information about client STAs being served
                 tx_bitrate_link = self.device.get_tx_bitrate_of_connected_devices(self.iface)
                 for sta_mac_addr, sta_speed in tx_bitrate_link.items():
-                    sta_tx_bitrate_val = sta_speed[0] # e.g. 12
+                    sta_tx_bitrate_val = float(sta_speed[0]) # e.g. 12
                     sta_tx_bitrate_unit = sta_speed[1] # e.g. Mbit/s
-
-                    # TODO: sven do something ...
-                    # mac_addr -> (rate, unit)
-                    pass
+                    print(str(sta_mac_addr)+": STA_TX_BITRATE PHY: "+str(sta_tx_bitrate_val)+" "+str(sta_tx_bitrate_unit))
+                    sta_tx_bitrate_val = sta_tx_bitrate_val * 0.85
+                    print(str(sta_mac_addr)+": STA_TX_BITRATE DATA: "+str(sta_tx_bitrate_val)+" "+str(sta_tx_bitrate_unit))
+                    if sta_tx_bitrate_unit == "MBit/s":
+                        self.phy_rates[sta_mac_addr] = sta_tx_bitrate_val
+                    else:
+                        print("CONVERT THE BITRATE!!!!!")
+                        # TODO write converter
 
                 # step 2: process link info & decide on new slice sizes
-                # TODO: sven do something ...
-
+                
+                for sta_mac_addr in self.phy_rates:
+                    if sta_mac_addr in self.min_rates.keys():
+                        print("STA is in min_rates")
+                        slot_bitrate = float(self.phy_rates[sta_mac_addr]) / float(self.total_slots)
+                        print("Slot Bitrate for STA: "+str(slot_bitrate))
+                        print("Policy Min Bitrate: "+str(self.min_rates[sta_mac_addr]))
+                        number_of_slots = round(self.min_rates[sta_mac_addr] / slot_bitrate + 0.5) 
+                        self.min_slots[sta_mac_addr]= number_of_slots
+                        print("Min Slots needed: "+str(self.min_slots[sta_mac_addr]))                     
 
                 # step 3: update hMAC
 
-                # TODO: adapt hMAC config
                 # assign access policies to each slot in superframe
-                for slot_nr in range(self.mac.getNumSlots()):
-                    ac_slot = self.mac.getAccessPolicy(slot_nr)
-                    ac_slot.disableAll()
+                #Count needed slots for primary user
+                total_used_slots = 0.0
+                for sta_mac_addr in self.min_slots:
+                    total_used_slots = total_used_slots + self.min_slots[sta_mac_addr]                    
+                if total_used_slots > self.total_slots:
+                    print("Warning!: More slots needed as available, target bitrate for all primary users not achievable! Needed: "+str(total_used_slots)+" Available: "+str(self.total_slots))
+                    total_used_slots = self.total_slots            
+                else:
+                    print("Total used slots for primary: "+str(total_used_slots)) 
+                self.mac.printConfiguration()
+                for slot_nr in range(0,int(total_used_slots)):
+                    print("Processing primary slot nr: "+str(slot_nr))  
+                    #ac_slot = self.mac.getAccessPolicy(slot_nr)
+                    ac_slot = HMACAccessPolicyParam()
+                    for sta_mac_addr in self.min_slots:
+                        print("Adding mac "+str(sta_mac_addr))
+                        ac_slot.addDestMacAndTosValues(sta_mac_addr, 0)
+                    self.mac.addAccessPolicy(slot_nr, ac_slot)
+                #for slot_nr in range(int(total_used_slots), int(self.total_slots)):
+                    # TODO Replace this with STAs of Guest Network
+                #    print("Processing secondary slot nr: "+str(slot_nr))
+                #    ac_slot = self.mac.getAccessPolicy(slot_nr)
+                #    ac_slot = ac_slot.disableAll()
+                    #self.mac.addAccessPolicy(slot_nr, ac_slot)
                     # TODO: sven do something ...
                     # node on which scheme should be applied, e.g. nuc15 interface sta1
-                    staDstHWAddr = "04:f0:21:17:36:68"
-                    ac_slot.addDestMacAndTosValues(staDstHWAddr, 0)
+                #    staDstHWAddr = "04:f0:21:17:36:68"
+                #    ac_slot.addDestMacAndTosValues(staDstHWAddr, 0)
 
                 # update configuration in hMAC
+                self.mac.printConfiguration()
                 self.device.update_radio_program(self.myHMACID, self.mac, self.iface)
+                #self.device.update_mac_processor(self.iface, self.mac)
 
-        except Exception as e:
-            self.log.error("{} Failed updating mac processor, err_msg: {}"
-                           .format(datetime.datetime.now(), e))
-            raise e
+        #except Exception as e:
+        #    self.log.error("{} Failed updating mac processor, err_msg: {}"
+        #                   .format(datetime.datetime.now(), e))
+        #    raise e
 
         self.timer.start(self.update_interval)
